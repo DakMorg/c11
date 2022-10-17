a1b25bd74380820ac52556ff8259bf8c6b413670
DakMorg <108765820+DakMorg@users.noreply.github.com>
2022-10-17 17:46:44 -0400

    heroku3

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 1234;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
