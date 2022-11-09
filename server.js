acd6d96a265c0ae5edde6ff88e77589e46d0be5e
DakMorg <108765820+DakMorg@users.noreply.github.com>
2022-11-08 21:20:05 -0500

    heroku fix

--- server.js
+++ server.js
@@ -6,8 +6,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-require('./routes/apiroutes')(app);
-require('./routes/htmlroutes')(app);
+/* require('./routes/apiroutes')(app);
+require('./routes/htmlroutes')(app); */
+
+app.use(require('./routes/apiRoutes'))
+app.use(require('./routes/htmlRoutes'))
 
 app.listen(PORT, function() {
   console.log(`Server listening on PORT: ${PORT}`);
